acff42fb414053bc6b49fabc4f2c92fb8dce4949
vigneshviggs <vigneshviggs100@gmail.com>
2019-09-06 19:22:06 -0400

    Commit

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody rb;
+	void Start ()
+	{
+
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+		
+	}
+	
+	
+}﻿
